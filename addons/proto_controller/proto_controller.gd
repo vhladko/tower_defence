36b291f47206f81fbc45fe7eb1b3b5052f12552a
vhladko <kf.gladko@gmail.com>
2025-05-08 15:50:23 +0300

    handle movements

---
+++ addons/proto_controller/proto_controller.gd
@@ -0,0 +1,79 @@
+# ProtoController v1.0 by Brackeys
+# CC0 License
+# Intended for rapid prototyping of first-person games.
+# Happy prototyping!
+
+extends CharacterBody3D
+
+@export_group("Speeds")
+## Look around rotation speed.
+@export var look_speed : float = 0.002
+## Normal speed.
+@export var base_speed : float = 25.0
+
+
+@export_group("Input Actions")
+## Name of Input Action to move Left.
+@export var input_left : String = "ui_left"
+## Name of Input Action to move Right.
+@export var input_right : String = "ui_right"
+## Name of Input Action to move Forward.
+@export var input_forward : String = "ui_up"
+## Name of Input Action to move Backward.
+@export var input_back : String = "ui_down"
+## Name of Input Action to Jump.
+
+var mouse_captured : bool = false
+var look_rotation : Vector2
+var move_speed : float = 0.0
+var freeflying : bool = true
+
+## IMPORTANT REFERENCES
+@onready var head: Node3D = $Head
+@onready var collider: CollisionShape3D = $Collider
+
+func _ready() -> void:
+	look_rotation.y = rotation.y
+	look_rotation.x = head.rotation.x
+
+func _unhandled_input(event: InputEvent) -> void:
+	# Mouse capturing
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		capture_mouse()
+	if Input.is_key_pressed(KEY_ESCAPE):
+		release_mouse()
+	
+	# Look around
+	if mouse_captured and event is InputEventMouseMotion:
+		rotate_look(event.relative)
+
+
+func _physics_process(delta: float) -> void:
+	# If freeflying, handle freefly and nothing else
+	var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
+	var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	motion *= base_speed * delta
+	move_and_collide(motion)
+	return
+
+## Rotate us to look around.
+## Base of controller rotates around y (left/right). Head rotates around x (up/down).
+## Modifies look_rotation based on rot_input, then resets basis and rotates by look_rotation.
+func rotate_look(rot_input : Vector2):
+	look_rotation.x -= rot_input.y * look_speed
+	look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
+	look_rotation.y -= rot_input.x * look_speed
+	transform.basis = Basis()
+	rotate_y(look_rotation.y)
+	head.transform.basis = Basis()
+	head.rotate_x(look_rotation.x)
+	
+
+func capture_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+
+
+func release_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mouse_captured = false
