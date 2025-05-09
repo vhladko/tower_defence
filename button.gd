84cb91d85f5b7f09e0ed229422eff7f97c84c436
Vlad Hladko <vladhladko@Vlads-MacBook-Pro.local>
2025-05-09 20:20:00 +0300

    add grid map

--- button.gd
+++ button.gd
@@ -5,8 +5,9 @@ extends Button
 var _is_dragging: bool = false
 var _building_instance: Node3D
 var _cam: Camera3D
+var _last_valid_location: Vector3
 
-var RAYCAST_LENGTH = 100.0
+var RAYCAST_LENGTH = 1000.0
 
 func _ready():
 	icon = building_icon
@@ -22,6 +23,7 @@ func _on_button_down():
 
 func _on_button_up():
 	_is_dragging = false
+	_building_instance.visible = false
 
 func _physics_process(_delta):
 	if _is_dragging:
@@ -31,8 +33,20 @@ func _physics_process(_delta):
 		var end = origin + _cam.project_ray_normal(mouse_position) * RAYCAST_LENGTH
 		var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(origin, end)
 		query.collide_with_areas = true
+		query.collide_with_bodies = true
+		query.collision_mask = 2
 		var ray_result = space_state.intersect_ray(query)
-		print(ray_result.size())
+		print(ray_result)
 		if ray_result.size() > 0:
+			_building_instance.visible = true
 			var col = ray_result.get('collider')
 			print(col)
+			var current_position = Vector3(col.global_position.x, 0.2, col.global_position.z)
+			_building_instance.global_position = current_position
+			var groups = col.get_groups()
+			if groups.size() == 0:
+				return
+			if groups[0] == 'building_area':
+				_last_valid_location = current_position
+		else:
+			_building_instance.global_position = Vector3(mouse_position.x, 0.2, mouse_position.y)
