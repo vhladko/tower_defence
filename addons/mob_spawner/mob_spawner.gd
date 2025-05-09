e6b5065fa7cc4d9fd63a3e3fb525f717f37ef07d
AnimeManna <maxuniwersal@gmail.com>
2025-05-09 18:59:20 +0300

    Added functionality to attack mobs in building

--- addons/mob_spawner/mob_spawner.gd
+++ addons/mob_spawner/mob_spawner.gd
@@ -4,9 +4,13 @@ extends Node3D
 
 @export var path: Path3D;
 
+var count: int = 0;
+
 func _on_timer_timeout() -> void:
-	var mob = mob_prefab.instantiate()
-	mob.path = path
-	mob.speed = 2.0
-	add_child(mob)
+	if count < 2:
+		var mob = mob_prefab.instantiate()
+		mob.path = path
+		mob.speed = 2.0
+		add_child(mob)
+		count += 1
 	
--- building/building.gd
+++ building/building.gd
@@ -1,2 +1,37 @@
 extends Node3D
 class_name Building
+
+var mobs_in_range = []
+var damage: float = 10.0
+var fire_rate: float = 1
+
+func _ready() -> void:
+	var timer = Timer.new()
+	timer.wait_time = fire_rate
+	timer.autostart = true
+	add_child(timer)
+	timer.connect("timeout", _on_timer_timeout)
+
+func _on_timer_timeout():
+	attack()
+
+func _on_attack_range_body_entered(body: Node3D) -> void:
+	if body.has_method("get_hit"):
+		mobs_in_range.append(body);
+
+
+func _on_attack_range_body_exited(body: Node3D) -> void:
+	if body in mobs_in_range:
+		mobs_in_range.erase(body)
+
+func attack():
+	if mobs_in_range.size() == 0:
+		return
+	mobs_in_range.sort_custom(_sort_by_distance)
+	var target = mobs_in_range[0]
+	if is_instance_valid(target):
+		target.get_hit(damage)
+
+
+func _sort_by_distance(a, b):
+	return a.pathFollow.progress > b.pathFollow.progress
