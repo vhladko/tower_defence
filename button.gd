c9a9c18a843bf7e0bbf1d519463720073e17d3ad
Vlad Hladko <vladhladko@Vlads-MacBook-Pro.local>
2025-05-10 14:05:30 +0300

    add transparents

--- button.gd
+++ button.gd
@@ -2,7 +2,6 @@ extends Button
 @export var building: PackedScene
 @export var building_icon: Texture2D
 @export var main: Node3D
-@export var meshLibrary: MeshLibrary
 
 var _is_dragging: bool = false
 var _building_instance: Node3D
@@ -15,17 +14,7 @@ func _ready():
 	icon = building_icon
 	_building_instance = building.instantiate()
 	_building_instance.is_ghost = true
-	var _building_model = _building_instance.get_child(0)
-	var _building_mesh = _building_model.get_child(0)
-	if _building_mesh is MeshInstance3D:
-		var mat = _building_mesh.get_surface_override_material(0)
-		print(mat)
-		if mat == null:
-			mat = StandardMaterial3D.new()
-			_building_mesh.set_surface_override_material(0, mat)
-		mat.transparency = 0.5
-		mat.albedo_color.a = 0.5 
-		mat.transparent = true
+	
 			
 	add_child(_building_instance)
 	_building_instance.visible = false
@@ -34,6 +23,7 @@ func _ready():
 
 func _on_button_down():
 	_is_dragging = true
+	make_building_transparent()
 
 
 func _on_button_up():
@@ -56,13 +46,66 @@ func _physics_process(_delta):
 		if ray_result.size() > 0:
 			_building_instance.visible = true
 			var col = ray_result.get('collider')
-			print(col)
 			if col is GridMap:
 				var local_coords = col.local_to_map(ray_result.get("position"))
-				# var item = col.get_cell_item(local_coords)
-				# print(item)
 				var global_coords = col.map_to_local(local_coords)
+				var item = col.get_cell_item(local_coords)
 				_building_instance.global_position = global_coords
 				_last_valid_location = global_coords
+				if item > 0:
+					var layers = col.mesh_library.get_item_navigation_layers(item)
+					var names = get_navigation_layer_name_from_mask(layers)
+
 		else:
 			_building_instance.global_position = Vector3(mouse_position.x, 0.2, mouse_position.y)
+
+func get_navigation_layer_name_from_mask(mask: int) -> Array:
+	var names := []
+	for i in 32:
+		if mask & (1 << i) != 0:
+			var layer_idx = i + 1
+			var setting = "layer_names/3d_navigation/layer_%d" % layer_idx
+			var layer_name = ProjectSettings.get(setting)
+			if layer_name == "":
+				layer_name = "Layer %d" % layer_idx
+			names.append(layer_name)
+	return names
+
+func make_building_transparent():
+	var _building_model = _building_instance.get_child(0)
+	var _building_mesh = _building_model.get_child(0)
+	if _building_mesh is MeshInstance3D:
+		var mat = _building_mesh.get_active_material(0)
+		if mat == null:
+			mat = StandardMaterial3D.new()
+		else:
+			mat = mat.duplicate()
+
+		_building_mesh.set_surface_override_material(0, mat)
+		mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+		mat.blend_mode = BaseMaterial3D.BLEND_MODE_ADD
+
+		var c: Color = mat.albedo_color
+		c.a = 0.2
+		mat.albedo_color = c
+
+func make_building_error():
+	var _building_model = _building_instance.get_child(0)
+	var _building_mesh = _building_model.get_child(0)
+	if _building_mesh is MeshInstance3D:
+		var mat = _building_mesh.get_active_material(0)
+		if mat == null:
+			mat = StandardMaterial3D.new()
+		else:
+			mat = mat.duplicate()
+
+		_building_mesh.set_surface_override_material(0, mat)
+		mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+		mat.blend_mode = BaseMaterial3D.BLEND_MODE_ADD
+
+		var c: Color = mat.albedo_color
+		c.a = 0.3
+		c.r = 1
+		c.b = 0
+		c.g = 0
+		mat.albedo_color = c
---
+++ grid_map.gd
@@ -0,0 +1,5 @@
+extends GridMap
+
+func _ready():
+	var mesh = mesh_library.get_item_navigation_layers(1)
+	print(mesh)
