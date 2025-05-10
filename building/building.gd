7d24f2cf5691b3cfc1f2cd7919cee44136f47178
Vlad Hladko <vladhladko@Vlads-MacBook-Pro.local>
2025-05-10 22:27:53 +0300

    add gold system

--- building/building.gd
+++ building/building.gd
@@ -1,47 +1,4 @@
-extends Node3D
-class_name Building
+extends BaseBuilding
 
-@export var bullet_prefab: PackedScene
 var bullet_speed: float = 30.0
 var bullet_damage: float = 10.0
-
-var is_ghost: bool = false
-
-var mobs_in_range = []
-var fire_rate: float = 0.5
-
-func _ready() -> void:
-	var timer = Timer.new()
-	timer.wait_time = fire_rate
-	timer.autostart = true
-	add_child(timer)
-	timer.connect("timeout", _on_timer_timeout)
-
-func _on_timer_timeout():
-	if not is_ghost:
-		attack()
-
-func _on_attack_range_body_entered(body: Node3D) -> void:
-	if body.has_method("get_hit"):
-		mobs_in_range.append(body);
-
-func _on_attack_range_body_exited(body: Node3D) -> void:
-	if body in mobs_in_range:
-		mobs_in_range.erase(body)
-
-func attack():
-	if mobs_in_range.size() == 0:
-		return
-	mobs_in_range.sort_custom(_sort_by_distance)
-	var target = mobs_in_range[0]
-	if is_instance_valid(target):
-		var bullet = bullet_prefab.instantiate()
-		bullet.speed = bullet_speed
-		bullet.damage = bullet_damage
-		bullet.target = target
-		bullet.global_transform.origin = bullet.global_transform.origin + Vector3(0, 3, 0)
-		add_child(bullet)
-
-
-func _sort_by_distance(a, b):
-	return a.pathFollow.progress > b.pathFollow.progress
--- game_state.gd
+++ game_state.gd
@@ -7,7 +7,7 @@ enum UI_MODES {GAME, BUILDING}
 
 var ui_mode: UI_MODES = UI_MODES.GAME
 var user_hp: int = 100
-var gold: float = 0
+var gold: float = 50
 
 func set_ui_mode(mode: UI_MODES) -> void:
 	ui_mode = mode
@@ -26,7 +26,7 @@ func _game_win():
 func _reset_state():
 	ui_mode = UI_MODES.GAME
 	user_hp = 100
-	gold = 0 
+	gold = 50
 	BuildManager.cancel_building()
 	
 func set_user_hp(hp: int) -> void:
@@ -42,6 +42,8 @@ func set_user_hp(hp: int) -> void:
 func add_gold(gold_to_add: float) -> void:
 	gold += gold_to_add
 
+func remove_gold(gold_to_remove: float) -> void:
+	gold -= gold_to_remove
+
 func waves_finished() -> void:
 	_game_win()
-	
