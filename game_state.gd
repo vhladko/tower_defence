a53746480f3b7ce2f29d2a8fa48cd0639817dbd3
Vlad Hladko <vladhladko@Vlads-MacBook-Pro.local>
2025-05-10 17:48:21 +0300

    add game over and restart

--- game_state.gd
+++ game_state.gd
@@ -1,12 +1,11 @@
 extends Node
 
+signal game_over
+
 enum UI_MODES {GAME, BUILDING}
 
 var ui_mode: UI_MODES = UI_MODES.GAME
 var user_hp: int = 100
-
-func _ready():
-	pass
 
 func set_ui_mode(mode: UI_MODES) -> void:
 	ui_mode = mode
@@ -14,9 +13,18 @@ func set_ui_mode(mode: UI_MODES) -> void:
 func get_ui_mode() -> UI_MODES:
 	return ui_mode
 
+func _game_over():
+	emit_signal("game_over")
+	_reset_state()
+
+func _reset_state():
+	ui_mode = UI_MODES.GAME
+	user_hp = 100
+
 func set_user_hp(hp: int) -> void:
-	if (hp < 0):
+	if (hp <= 0):
 		user_hp = 0
+		_game_over()
 		return
 	if (hp > 100):
 		user_hp = 100
