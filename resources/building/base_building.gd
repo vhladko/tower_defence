b6b17a51afd73d321aed498a3daf21e9f2ee99f8
Vlad Hladko <vladhladko@Vlads-MacBook-Pro.local>
2025-05-11 14:54:33 +0300

    add selection

--- resources/building/base_building.gd
+++ resources/building/base_building.gd
@@ -5,11 +5,14 @@ class_name BaseBuilding
 
 var bullet_prefab: PackedScene
 var fire_rate: float
+var _is_selected: bool = false
+var _cam: Camera3D
 
 var is_ghost: bool = false
 var mobs_in_range = []
 
 func _ready() -> void:
+	_cam = get_viewport().get_camera_3d()
 	bullet_prefab = building_data.bullet_prefab
 	fire_rate = building_data.fire_rate
 	var attack_range = create_attack_range(building_data.attack_range_radius)
@@ -20,6 +23,23 @@ func _ready() -> void:
 	timer.autostart = true
 	add_child(timer)
 	timer.connect("timeout", _on_timer_timeout)
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		var space_state = get_tree().get_root().get_world_3d().direct_space_state
+		var mouse_position = get_viewport().get_mouse_position()
+		var origin = _cam.project_ray_origin(mouse_position)
+		var end = origin + _cam.project_ray_normal(mouse_position) * 1000
+		var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(origin, end)
+		query.collide_with_areas = true
+		query.collide_with_bodies = true
+		query.collision_mask = 8
+		var ray_result = space_state.intersect_ray(query)
+		if ray_result.size() > 0:
+			var col = ray_result.get('collider')
+			if col is StaticBody3D:
+				_is_selected = is_ancestor_of(col)
+
 
 func _on_timer_timeout():
 	if not is_ghost:
@@ -38,14 +58,13 @@ func create_attack_range(radius: float) -> Area3D:
 	attack_range.collision_layer = (1 << 0) | (1 << 2)
 	attack_range.collision_mask = 1
 	
-	attack_range.connect("body_entered",  _on_attack_range_body_entered)
-	attack_range.connect("body_exited",  _on_attack_range_body_exited)
+	attack_range.connect("body_entered", _on_attack_range_body_entered)
+	attack_range.connect("body_exited", _on_attack_range_body_exited)
 	
 	return attack_range
 
 
 func _on_attack_range_body_entered(body: Node3D) -> void:
-	print('collision')
 	if body.has_method("get_hit"):
 		mobs_in_range.append(body);
 
@@ -68,3 +87,18 @@ func attack():
 
 func _sort_by_distance(a, b):
 	return a.pathFollow.progress > b.pathFollow.progress
+
+func check_selection():
+	var space_state = get_tree().get_root().get_world_3d().direct_space_state
+	var mouse_position = get_viewport().get_mouse_position()
+	var origin = _cam.project_ray_origin(mouse_position)
+	var end = origin + _cam.project_ray_normal(mouse_position) * 1000
+	var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+	query.collide_with_bodies = true
+	query.collision_mask = 8
+	var ray_result = space_state.intersect_ray(query)
+	if ray_result.size() > 0:
+		var col = ray_result.get('collider')
+		if col is StaticBody3D:
+			_is_selected = is_ancestor_of(col)
